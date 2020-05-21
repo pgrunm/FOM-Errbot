5fb2ff51b414cf6fe34f9d05c4de94e63878ac6a
Warryz <8317771+Warryz@users.noreply.github.com>
2020-05-21 11:36:55 +0200

    Added a basic config file.

---
+++ config.py
@@ -0,0 +1,16 @@
+import logging
+
+BACKEND = 'Text'
+
+BOT_DATA_DIR = r'./data'
+BOT_EXTRA_PLUGIN_DIR = r'./plugins'
+
+BOT_LOG_FILE = r'./errbot.log'
+BOT_LOG_LEVEL = logging.DEBUG
+
+# Bot Administrators ID
+BOT_ADMINS = (123456789, )
+
+BOT_IDENTITY = {
+    'token': '103419016:AAbcd1234...',
+}
